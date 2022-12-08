245e4cb0cbb89b0e050ab48f532a44ad9da153fa
Washington Mendes dos Santos <washington.mendes@ibm.com>
2022-12-08 19:42:03 -0300

    Adicionado comentário com infromação da lista de usuários

--- src/server.js
+++ src/server.js
@@ -5,6 +5,17 @@ const PORT = 3000
 
 app.use(express.json())
 
+//Esse é o formato de informações esperado que o usuário tenha
+// const user = {
+//     id: number,
+//     nome: string,
+//     nascimento: Date,
+//     livrosAlugados:[]
+// }
+
+//Lista de usuários você precisa cadastrar um objeto de usuário na lista pois essa lista deve conter uma coleção de usuários.
+//let usuarios = [];
+
 app.get('/', (req, res) => {
     return res.status(400).json({message: 'Hello World'})
 })
