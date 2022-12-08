04fa0b41f5a38b8d37b65912082e2ed9115e72ba
Washington Mendes dos Santos <washington.mendes@ibm.com>
2022-12-08 20:01:16 -0300

    Adicionado vetor de usuário e rotas para manipulação

--- src/server.js
+++ src/server.js
@@ -1,45 +1,83 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-//Esse é o formato de informações esperado que o usuário tenha
-// const user = {
-//     id: number,
-//     nome: string,
-//     nascimento: Date,
-//     livrosAlugados:[]
-// }
+let id = 0;
+let usuarios = [];
 
-//Lista de usuários você precisa cadastrar um objeto de usuário na lista pois essa lista deve conter uma coleção de usuários.
-//let usuarios = [];
+app.get("/", (req, res) => {
+  return res.status(200).json({ usuarios });
+});
 
-app.get('/', (req, res) => {
-    return res.status(400).json({message: 'Hello World'})
-})
+app.post("/", (req, res) => {
+  const { nome, nascimento } = req.body;
+  if (id === 0) {
+    const user = {
+      id,
+      nome,
+      nascimento,
+      livrosAlugados: [],
+    };
+    usuarios.push(user);
+  } else {
+    id = id + 1;
+    const user = {
+      id,
+      nome,
+      nascimento,
+      livrosAlugados: [],
+    };
+    usuarios.push(user);
+  }
+  const newUser = usuarios.find((user) => user.id === id);
+  if (!newUser) {
+    throw new Error("usuários não encontrado");
+  }
+  return res.status(201).json(newUser);
+});
 
-app.post('/', (req, res) => {
-    const { nome } = req.body
-    return res.status(201).json(nome)
-})
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = usuarios.find((user) => user.id === id);
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuário não encontrado!",
+    });
+  }
+  return res.status(200).json(nome);
+});
 
-app.get('/:nome', (req, res) => {
-    const {nome} = req.params
-    return res.status(200).json(nome)
-})
+app.put("/:id", (req, res) => {
+  const { nome, nascimento } = req.params;
+  const user = usuarios.find((user) => user.id === id);
 
-app.get('/livros/:nomeLivro', (req, res) => {
-    const {nomeLivro} = req.params
-    return res.status(200).json({nomeLivro})
-})
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuário não encontrado!",
+    });
+  }
+  user.nome = nome;
+  user.nascimento = nascimento;
+  user.livrosAlugados = [];
 
-app.get('/autor/:autor', (req, res) => {
-    const { autor } = req.params
-    return res.status(200).json({autor})
-})
+  return res.status(200).json(user);
+});
+
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const user = usuarios.find((user) => user.id === id);
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuário não encontrado!",
+    });
+  }
+  return res.status(200).json(user);
+});
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-})
+  console.log(`http://localhost:${PORT}`);
+});
